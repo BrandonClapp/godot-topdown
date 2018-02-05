d7028907c828e7b23ddb4300967243d192c42f4f
Brandon Clapp <brandonclapp@outlook.com>
2018-02-05 09:05:53 -0600

    Doors

--- Player.cs
+++ Player.cs
@@ -6,21 +6,28 @@ public class Player : KinematicBody2D
     private Vector2 _velocity;
     private Vector2 _screenSize;
     private int _speed = 300;
+    private AnimatedSprite _sprite;
+    private bool _isInteractable;
+    private int _dest_x;
+    private int _dest_y;
 
     public override void _Ready()
     {
         // Called every time the node is added to the scene.
         // Initialization here
         _screenSize = GetViewportRect().Size;
-        //_collision = (CollisionShape2D)GetNode("Collision");
+        _sprite = (AnimatedSprite)GetNode("Sprite");
         Position = _screenSize / 2;
     }
 
     public override void _Process(float delta)
     {
-        // Called every frame. Delta is time since last frame.
-        // Update game logic here.
+        ProcessMovement(delta);
+        ProcessInteraction();
+    }
 
+    private void ProcessMovement(float delta)
+    {
         if (Input.IsActionPressed("ui_up"))
         {
             _velocity = new Vector2(0, -1);
@@ -42,14 +49,78 @@ public class Player : KinematicBody2D
             _velocity = new Vector2();
         }
 
+        if (_velocity.x > 0)
+        {
+            //_sprite.Play("right");
+            //_sprite.FlipH = false;
+            _sprite.RotationDegrees = 0;
+        }
+        else if (_velocity.x < 0)
+        {
+            //_sprite.Play("right");
+            //_sprite.FlipH = true;
+            _sprite.RotationDegrees = -180;
+        }
+
+        if (_velocity.y > 0)
+        {
+            //_sprite.Play("up");
+            //_sprite.FlipV = true;
+            _sprite.RotationDegrees = 90;
+        }
+        else if (_velocity.y < 0)
+        {
+            //_sprite.Play("up");
+            //_sprite.FlipV = false;
+            _sprite.RotationDegrees = -90;
+        }
+
         var newPos = _velocity * _speed * delta;
         MoveAndCollide(newPos);
+    }
 
+    private void ProcessInteraction()
+    {
+        if (Input.IsActionJustPressed("ui_interact"))
+        {
+            if (_isInteractable)
+            {
+                GD.Print($"Warping to {_dest_x}x{_dest_y}");
+                Warp(_dest_x, _dest_y);
+                _dest_x = 0;
+                _dest_y = 0;
+            }
+        }
+
+        
+    }
+
+    private void _on_Interaction_area_shape_entered(int area_id, Godot.Object area, int area_shape, int self_shape)
+    {
+        GD.Print($"Entered area. {area} - {area_shape} - {self_shape}");
+        var map = (string)area.GetMeta("dest_map");
+        var x = (int)area.GetMeta("dest_tile_x");
+        var y = (int)area.GetMeta("dest_tile_y");
+
+        GD.Print($"Tele to: {x}x{y}");
+        _isInteractable = true;
+
+        _dest_x = x;
+        _dest_y = y;
+    }
+
+    private void _on_Interaction_area_shape_exited(int area_id, Godot.Object area, int area_shape, int self_shape)
+    {
+        GD.Print($"Exited area. {area} - {area_shape} - {self_shape}");
+        _isInteractable = false;
+        _dest_x = 0;
+        _dest_y = 0;
+    }
+
+    private void Warp(int x, int y)
+    {
+        Visible = false;
+        Position = new Vector2(x, y);
+        Visible = true;
     }
 }
-
-
-
-
-
-
--- main.cs
+++ main.cs
@@ -4,85 +4,17 @@ using System;
 
 public class main : Node
 {
-    PackedScene _gem = ResourceLoader.Load(@"res://Gem.tscn") as PackedScene;
     Node _player;
-    Node _gemContainer;
     Vector2 _screenSize;
-    Label _scoreLabel;
-    Label _timeLabel;
-    Label _gameOver;
-    Button _gameOverRetry;
-    Timer _gameTimer;
-    int _score = 0;
-    int _level = 1;
 
     public override void _Ready()
     {
-        // Called every time the node is added to the scene.
-        // Initialization here
-        //AddChild(_player.Instance());
         _screenSize = GetViewport().GetVisibleRect().Size;
-        _gemContainer = GetNode("Gem_Container");
         _player = GetNode("Player");
-        _scoreLabel = (Label)GetNode("HUD/Score_Label");
-        _timeLabel = (Label)GetNode("HUD/Time_Label");
-        _gameTimer = (Timer)GetNode("Game_Timer");
-        _gameOverRetry = (Button)GetNode("HUD/GameOver_Retry_Button");
-        //SpawnGems(_level);
-    }
-
-    private void SpawnGems(int level)
-    {
-        for (var i = 0; i < level * 10; i++)
-        {
-            var g = _gem.Instance() as Area2D;
-            _gemContainer.AddChild(g);
-            g.Connect("gem_grabbed", this, nameof(GemGrabbed));
-            g.Position = new Vector2(
-                UniqueRandom.Rand(30, _screenSize.x - 30),
-                UniqueRandom.Rand(30, _screenSize.y - 30)
-                );
-        }
     }
 
     public override void _Process(float delta)
     {
-        _timeLabel.Text = $"Time: {((int)_gameTimer.GetTimeLeft()).ToString()}";
-        _scoreLabel.Text = $"Score: {_score.ToString()}";
-    }
 
-    private void GemGrabbed()
-    {
-        _score += 10;        
-    }
-
-    private void ResetGame()
-    {
-        _gameOver.Visible = false;
-        _gameOverRetry.Visible = false;
-        _gameTimer.Start();
-        _score = 0;
-        _player.SetProcess(true);
-        var gems = _gemContainer.GetChildren();
-        foreach (var gem in gems)
-        {
-            ((Node)gem).QueueFree();
-        }
-        _level = 1;
-        SpawnGems(1);
-    }
-
-    private void _on_Game_Timer_timeout()
-    {
-        _player.SetProcess(false);
-        _gameOver = (Label)GetNode("HUD/GameOver_Label");
-        _gameOverRetry = (Button)GetNode("HUD/GameOver_Retry_Button");
-        _gameOverRetry.Visible = true;
-        _gameOver.Visible = true;
-    }
-
-    private void _on_GameOver_Retry_Button_pressed()
-    {
-        ResetGame();
     }
 }
